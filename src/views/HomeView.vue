8f001b8007cbeef8e79e3d7f7bfadc1f89f2b49a
NursultanFront <nursultan020395@gmail.com>
2024-02-21 16:58:27 +0500

    feat: make custom buttoon

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import TheInput from '@/components/ui/input/TheInput.vue'
+import TheButton from '@/components/ui/button/TheButton.vue'
 const login = ref('')
 const password = ref('')
 </script>
@@ -10,6 +11,7 @@ const password = ref('')
     <form>
       <TheInput v-model="login" placeholder="Логин" />
       <TheInput v-model="password" placeholder="Пароль" />
+      <TheButton>Войти</TheButton>
     </form>
   </main>
 </template>
