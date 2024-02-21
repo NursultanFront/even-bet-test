9b3a0eb8a468b27d253d9849eca1e568dc6eb69b
NursultanFront <nursultan020395@gmail.com>
2024-02-21 16:47:19 +0500

    feat: make input custom component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import TheInput from '@/components/ui/input/TheInput.vue'
+const login = ref('')
+const password = ref('')
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <form>
+      <TheInput v-model="login" placeholder="Логин" />
+      <TheInput v-model="password" placeholder="Пароль" />
+    </form>
+  </main>
 </template>
