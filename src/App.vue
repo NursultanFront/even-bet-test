96cdb5fe876ca1c4f84f0f4bcf2951a721a8851f
NursultanFront <nursultan020395@gmail.com>
2024-02-23 19:30:48 +0500

    feat: finish the task, add setInterval to refresh token and balance

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import { useUserStore } from '@/stores/user'
-
-const userStore = useUserStore()
-const UPDATE_TIME = 800 * 1000
-
-// setInterval(() => {
-//   if (userStore.isAuth) {
-//     userStore.refreshToken()
-//   }
-// }, UPDATE_TIME)
 </script>
 
 <template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onMounted, onUnmounted, ref, watch } from 'vue'
 import { useBalanceStore } from '@/stores/balance'
 import { useGamesStore } from '@/stores/games'
+import { useUserStore } from '@/stores/user'
 import TheSidebar from '@/components/main/sidebar/TheSidebar.vue'
 import PlayCard from '@/components/main/play-card/PlayCard.vue'
 import GameModalVue from '@/components/main/game-modal/GameModal.vue'
@@ -12,9 +13,25 @@ const gameStore = useGamesStore()
 const isVisible = ref(false)
 const gameLink = ref('')
 
+const userStore = useUserStore()
+const UPDATE_TIME = 800 * 1000
+const BALANCE_UPDATE_TIME = 30 * 1000
+
+let tokenRefreshInterval: ReturnType<typeof setInterval> | null = null
+let balanceRefreshInterval: ReturnType<typeof setInterval> | null = null
+
 onMounted(() => {
   balanceStore.getBalance()
   gameStore.getGames()
+
+  tokenRefreshInterval = setInterval(userStore.refreshToken, UPDATE_TIME)
+
+  balanceRefreshInterval = setInterval(balanceStore.getBalance, BALANCE_UPDATE_TIME)
+})
+
+onUnmounted(() => {
+  if (tokenRefreshInterval !== null) clearInterval(tokenRefreshInterval)
+  if (balanceRefreshInterval !== null) clearInterval(balanceRefreshInterval)
 })
 
 const getGame = (id: string) => {
@@ -25,11 +42,18 @@ const getGame = (id: string) => {
       gameLink.value = res[0].attributes['launch-options']['game-url']
     })
     .catch(() => {
+      gameLink.value = 'Произошла ошибка'
       throw new Error('Произошла ошибка')
     })
 
   isVisible.value = true
 }
+
+watch(isVisible, () => {
+  if (!isVisible.value) {
+    gameLink.value = ''
+  }
+})
 </script>
 
 <template>
